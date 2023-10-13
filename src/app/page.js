734ed48095757b7ace3de4e99078576217ceb87f
@vaidehishrivastava09 <Vaidehishrivastava09@gmail.com>
2023-10-13 15:44:02 +0530

    Pet - specific page added and about a pet integrated

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
 "use client";
-import Image from "next/image";
-import Landing from "@/pages/Landing/page";
+
+import AllPets from "./pet/page";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-around p-24">
       <h1 style={{ color: "#fff" }}>Who is your spirit pet animal ?</h1>
-      <Landing />
+      <AllPets />
     </main>
   );
 }
